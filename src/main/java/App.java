9e5ac1e6fdcdbfb5d353593fd6a5d57ac4729c54
Jon <admiralorbiter@gmail.com>
2020-08-02 17:45:57 -0500

    Inital work on screenshot button

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import static java.awt.GraphicsDevice.WindowTranslucency.*;
 
 public class App extends JFrame implements MouseListener {
     Test_Automation automation = new Test_Automation();
@@ -11,15 +12,34 @@ public class App extends JFrame implements MouseListener {
     ScriptingArea scriptingArea = new ScriptingArea();
     FeatureBar featureBar = new FeatureBar();
 
-    public App(){
-        init();
+    private int clicks=0;
+    private Rectangle screenshotArea=new Rectangle();
+
+    public App(boolean transparent){
+        if(!transparent)
+            init();
+        else
+            initTransparent();
+    }
+
+    public void initTransparent(){
+        setUndecorated(true);
+        featureBar.setCurrentFeatureSelection("screenshotMode");
+
+        add(new JPanel());
+        setSize(1600, 900);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        addMouseListener(this);
+        setOpacity(.1f);
     }
 
     private void init(){
         setTitle("Test Automation");
         setJMenuBar(topMenu);
-        //add(automation, BorderLayout.CENTER);
-        add(scriptingArea, BorderLayout.CENTER);
+        //add(scriptingArea, BorderLayout.CENTER);
         add(automation, BorderLayout.CENTER);
         scriptingArea.setVisible(true);
         automation.setVisible(false);
@@ -33,21 +53,40 @@ public class App extends JFrame implements MouseListener {
         addMouseListener(this);
     }
 
-    public void run(){
+    public String run(){
         topMenu.update(scriptingArea);
+        String feature=featureBar.getCurrentFeatureSelection();
+        //System.out.print(feature);
+        if(feature.equals("scripting")){
+            //scriptingArea.setVisible(true);
+            //automation.setVisible(false);
+        }else if(feature.equals("screenshotMode")){
+            if(isUndecorated()){
+                feature="taking_screenshot";
+                featureBar.setCurrentFeatureSelection(feature);
+                System.out.println("Setting Opacity");
+            }
+        }else if(feature.equals("taking_screenshot")){
 
-        if(featureBar.getCurrentFeatureSelection().equals("scripting")){
-            scriptingArea.setVisible(true);
-            automation.setVisible(false);
         }
         repaint();
+        return feature;
     }
 
     public static void main(String[] args)
     {
-        App app = new App();
-        while(true)
-            app.run();
+        App app = new App(false);
+
+        while(true){
+            String feature=app.run();
+            if(feature.equals("screenshotMode")){
+                app.dispose();
+                app = new App(true);
+            }else if(feature.equals("programMode")){
+                app.dispose();
+                app = new App(false);
+            }
+        }
     }
 
     @Override
@@ -55,15 +94,33 @@ public class App extends JFrame implements MouseListener {
         System.out.println(e.getPoint());
         //Check to make sure it is within the filelist width
         String filepath=null;
-        if(e.getPoint().x<=300)
-            filepath=fileList.checkmouseclick(e.getPoint());
 
-        if(filepath!=null){
-            System.out.println(filepath+" picked from list.");
-            automation.openImage(fileParser.openImage(filepath));
-            featureBar.setCurrentFeatureSelection("screenshot");
-            automation.setVisible(true);
-            scriptingArea.setVisible(false);
+        if(featureBar.getCurrentFeatureSelection().equals("taking_screenshot")){
+            if(clicks==0){
+                screenshotArea=new Rectangle();
+                screenshotArea.x=e.getPoint().x;
+                screenshotArea.y=e.getPoint().y;
+                clicks++;
+            }else{
+                if(e.getPoint().x>=screenshotArea.x && e.getPoint().y>=screenshotArea.y){
+                    screenshotArea.width=e.getPoint().x-screenshotArea.x;
+                    screenshotArea.height=e.getPoint().y-screenshotArea.y;
+                    Screenshot.takeScreenshot("test_button", screenshotArea, fileParser);
+                }else{
+                    System.out.println("Error - x and/or y not greater than first point, try again");
+                }
+                featureBar.setCurrentFeatureSelection("programMode");
+                clicks=0;
+            }
+        }else{
+            if(e.getPoint().x<=300)
+                filepath=fileList.checkmouseclick(e.getPoint());
+
+            if(filepath!=null){
+                System.out.println(filepath+" picked from list.");
+                automation.openImage(fileParser.openImage(filepath));
+                featureBar.setCurrentFeatureSelection("viewing_screenshot");
+            }
         }
     }
 
--- src/main/java/FeatureBar.java
+++ src/main/java/FeatureBar.java
@@ -5,14 +5,20 @@ import java.awt.event.ActionListener;
 
 public class FeatureBar extends JPanel implements ActionListener {
     JButton scripting_area = new JButton("Scripting Area");
+    JButton screenshot_button = new JButton("screenshot_button");
     private String currentFeatureSelection="scripting";
     public FeatureBar(){
         scripting_area.setVerticalTextPosition(AbstractButton.CENTER);
         scripting_area.setHorizontalTextPosition(AbstractButton.LEADING);
         scripting_area.setActionCommand("scripting");
         scripting_area.addActionListener(this);
+        screenshot_button.setVerticalTextPosition(AbstractButton.CENTER);
+        screenshot_button.setHorizontalTextPosition(AbstractButton.LEADING);
+        screenshot_button.setActionCommand("taking_screenshot");
+        screenshot_button.addActionListener(this);
         setLayout(new FlowLayout(FlowLayout.LEADING));
         add(scripting_area);
+        add(screenshot_button);
     }
 
     public String getCurrentFeatureSelection(){
@@ -20,6 +26,8 @@ public class FeatureBar extends JPanel implements ActionListener {
     }
 
     public void setCurrentFeatureSelection(String featureSelection){
+        if(featureSelection!=currentFeatureSelection)
+            System.out.println("Feature change to "+featureSelection);
         this.currentFeatureSelection=featureSelection;
     }
 
@@ -27,6 +35,9 @@ public class FeatureBar extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("scripting")){
             currentFeatureSelection="scripting";
+        }else if(e.getActionCommand().equals("taking_screenshot")){
+            currentFeatureSelection="screenshotMode";
+            System.out.println(currentFeatureSelection);
         }
     }
 }
