604255dcf9076c1d31df5001470a9b4eb64b0c71
Jon <admiralorbiter@gmail.com>
2020-08-11 21:58:55 -0500

    Working on implementing opencv for template matching

--- src/main/java/AutomationFeatures.java
+++ src/main/java/AutomationFeatures.java
@@ -1,8 +1,39 @@
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 
 public final class AutomationFeatures {
-    public static boolean findScreenshot(){return false;}
+    public static boolean findScreenshot(String filePath){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Mat template=null;
+        BufferedImage image=null;
+
+        try {
+            Robot r = new Robot();
+
+            // Used to get ScreenSize and capture image
+            Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            image = r.createScreenCapture(capture);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        Mat source = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
+        byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        source.put(0, 0, data);
+
+        template=Imgcodecs.imread(filePath);
+
+
+
+        return false;}
     public static boolean findText(){return false;}
     public static boolean wait(int seconds){
         System.out.println("WaitFor: "+seconds);
